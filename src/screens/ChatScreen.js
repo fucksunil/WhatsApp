b5fc53bdf7adc440100c2aa9173b336cf182d8fc
Priyanka <priyanka2001@gmail.com>
2022-11-04 20:37:22 +0530

    UI-6 Input Box

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,24 +1,33 @@
-import { Text, View, ImageBackground, StyleSheet, FlatList } from 'react-native';
+import { ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from 'react-native';
 import bg from "../../assets/images/BG.png";
 import Message from '../components/Message';
 import messages from '../../assets/data/messages.json'
+import InputBox from '../components/InputBox';
 
 const ChatScreen = () => {
     return (
-        <ImageBackground source={bg} style={styles.bg}>
-            <FlatList 
-                data={messages}
-                renderItem={({item})=> <Message message={item} />}
-                style={styles.list} 
-                inverted/>
-        </ImageBackground>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.bg}
+        >
+            <ImageBackground source={bg} style={styles.bg}>
+                <FlatList
+                    data={messages}
+                    renderItem={({ item }) => <Message message={item} />}
+                    style={styles.list}
+                    inverted
+                />
+                <InputBox />
+
+            </ImageBackground>
+        </KeyboardAvoidingView>
     )
 
 }
 
 const styles = StyleSheet.create({
     bg: {
-        flex:1
+        flex: 1
     },
     list: {
         padding: 10,
