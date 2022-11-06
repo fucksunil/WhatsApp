b227873cb38a98576c6b8b43816823e2c6630057
Priyanka <priyanka2001@gmail.com>
2022-11-06 19:22:39 +0530

    BE-2 Authentication

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,9 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import ChatScreen from '../screens/ChatScreen';
-import ChatsScreen from '../screens/ChatsScreen';
 import MainTabNavigator from './MainTabNavigator';
+import ChatScreen from '../screens/ChatScreen';
+import ContactsScreen from "../screens/ContactsScreen";
 
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +14,7 @@ const Navigator = () => {
         <Stack.Navigator screenOptions={{headerStyle: {backgroundColor: 'whitesmoke'}}}>
             <Stack.Screen name="Home" component={MainTabNavigator} options={{headerShown: false}} />
             <Stack.Screen name="Chat" component={ChatScreen} />
+            <Stack.Screen name="Contacts" component={ContactsScreen} />
         </Stack.Navigator>
     </NavigationContainer>
   )
